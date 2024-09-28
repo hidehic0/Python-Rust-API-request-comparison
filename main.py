69c560731d88b9df7115db29c15b137e4fda5afc
hidehic0 <hidehico.ooba@gmail.com>
2024-09-28 20:46:29 +0900

    commit

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ url = "https://pokeapi.co/api/v2/pokemon/987"
 res = json.loads(requests.get(url=url).text)
 # echo あれ種族値だ　妥協したんじゃなかったけwww
 # echo Pythonはめちゃくちゃ型が柔軟なのでできたよ
+# echo あっそ
 for stats, p in zip(res["stats"], ["HP", "攻撃", "防御", "特攻", "特防", "素早さ"]):
     print(f'{p}:{stats["base_stat"]}')
