68a70871d8f08c2b7488ecf946e24d92b8b57e18
hidehic0 <hidehico.ooba@gmail.com>
2024-09-28 20:45:22 +0900

    commit

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ res = json.loads(requests.get(url=url).text)
 # echo あれ種族値だ　妥協したんじゃなかったけwww
 # echo Pythonはめちゃくちゃ型が柔軟なのでできたよ
 for stats, p in zip(res["stats"], ["HP", "攻撃", "防御", "特攻", "特防", "素早さ"]):
-    print(f'p:{stats["base_stat"]}')
+    print(f'{p}:{stats["base_stat"]}')
