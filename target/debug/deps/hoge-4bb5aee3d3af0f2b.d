7cadf40c2b2e1907e43af9ad9ee8d371fe160806
hidehic0 <hidehico.ooba@gmail.com>
2024-09-29 10:14:50 +0900

    commit

--- target/debug/deps/hoge-4bb5aee3d3af0f2b.d
+++ target/debug/deps/hoge-4bb5aee3d3af0f2b.d
@@ -1,5 +1,5 @@
-/home/hidehico/Documents/PythonRustAPIComparison/target/debug/deps/libhoge-4bb5aee3d3af0f2b.rmeta: main.rs
+/home/hidehico/Documents/Python-Rust-API-request-comparison/target/debug/deps/libhoge-4bb5aee3d3af0f2b.rmeta: main.rs
 
-/home/hidehico/Documents/PythonRustAPIComparison/target/debug/deps/hoge-4bb5aee3d3af0f2b.d: main.rs
+/home/hidehico/Documents/Python-Rust-API-request-comparison/target/debug/deps/hoge-4bb5aee3d3af0f2b.d: main.rs
 
 main.rs:
--- target/debug/deps/hoge-5311916517935ccc.d
+++ target/debug/deps/hoge-5311916517935ccc.d
@@ -1,5 +1,5 @@
-/home/hidehico/Documents/PythonRustAPIComparison/target/debug/deps/libhoge-5311916517935ccc.rmeta: main.rs
+/home/hidehico/Documents/Python-Rust-API-request-comparison/target/debug/deps/libhoge-5311916517935ccc.rmeta: main.rs
 
-/home/hidehico/Documents/PythonRustAPIComparison/target/debug/deps/hoge-5311916517935ccc.d: main.rs
+/home/hidehico/Documents/Python-Rust-API-request-comparison/target/debug/deps/hoge-5311916517935ccc.d: main.rs
 
 main.rs:
