b40a3eff0f17fe024654cebc447a49902bf57275
hidehic0 <hidehico.ooba@gmail.com>
2024-09-29 09:42:33 +0900

    commit

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import requests
 import json
 import matplotlib.pyplot as plt
-import numpy as np
-import japanize_matplotlib
+import numpy as np  # type: ignore
+import matplotlib
+
 
 # APIを叩く
 url = "https://pokeapi.co/api/v2/pokemon/987"
@@ -21,7 +22,7 @@ value_list = []
 for stats, p in zip(res["stats"], ["HP", "攻撃", "防御", "特攻", "特防", "素早さ"]):
     print(f'{p}:{stats["base_stat"]}')
     label_list.append(p)
-    value_list.append(stats)
+    value_list.append(stats["base_stat"])
 
 value_list += value_list[:1]
 
@@ -34,4 +35,4 @@ fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
 ax.plot(angle_list, value_list, linewidth=2, linestyle="solid")
 ax.fill(angle_list, value_list, "b", alpha=0.1)
 
-plt.show()
+plt.savefig("chart.png")
